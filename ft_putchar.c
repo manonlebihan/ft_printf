ce77224b6520767359a51fe78c78be329b2cb29c
Manon Le Bihan <manonlebihan1999@gmail.com>
2022-06-21 16:43:16 +0200

    TODO

--- ft_putchar.c
+++ ft_putchar.c
@@ -1,31 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_putchar.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mle-biha <mle-biha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/21 15:00:39 by mle-biha          #+#    #+#             */
-/*   Updated: 2022/06/21 15:49:13 by mle-biha         ###   ########.fr       */
+/*   Created: 2022/06/21 15:49:31 by mle-biha          #+#    #+#             */
+/*   Updated: 2022/06/21 15:49:32 by mle-biha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdarg.h>
-
-int    ft_printf(const char *s, ...)
-{
-	va_list	args;
-
-	va_start(args, s);
-
-	
-}
-
-int	main(void)
-{
-	char	c;
-
-	c = 'm';
-	printf("%c", c);
-}
