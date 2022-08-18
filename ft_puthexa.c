43f4545d2e26e25e1b91680831cb41f19b3d1159
Manon Le Bihan <manonlebihan1999@gmail.com>
2022-08-18 11:06:51 +0200

    ft_puthexa without main

--- ft_puthexa.c
+++ ft_puthexa.c
@@ -6,7 +6,7 @@
 /*   By: mle-biha <mle-biha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 14:23:12 by mle-biha          #+#    #+#             */
-/*   Updated: 2022/08/18 11:05:54 by mle-biha         ###   ########.fr       */
+/*   Updated: 2022/08/18 11:06:42 by mle-biha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,18 +31,4 @@ int	ft_puthexa(size_t nb, char *base)
 		i += ft_puthexa(nb % 16, base);
 	}
 	return (i);
-}
-
-int	main(int argc, char *argv[])
-{
-	int	v = -230299;
-
-	if (argc == 3)
-	{
-		printf("puthexa : \n");
-		ft_puthexa(atoi(argv[1]), argv[2]);
-		ft_putchar('\n');
-		printf("printf : \n");
-		printf("%X\n", v);
-	}
 }
