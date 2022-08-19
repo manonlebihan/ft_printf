b07b72905074fb812dcc94e075a73b7417cd5233
manonlebihan <manonlebihan1999@gmail.com>
2022-08-19 15:24:18 +0200

    norm, OK

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mle-biha <mle-biha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 15:00:39 by mle-biha          #+#    #+#             */
-/*   Updated: 2022/08/19 14:57:38 by mle-biha         ###   ########.fr       */
+/*   Updated: 2022/08/19 15:24:00 by mle-biha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,14 +35,3 @@ int	ft_printf(const char *s, ...)
 	va_end(args);
 	return (size);
 }
-
-/* int	main(void)
-{
-	char	*s2;
-	s2 = "Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Atirei o pau no gatis, per gatis num morreus.";
-	printf("PRINTF\n");
-	printf(" %s %s %s %s %s\n", " - ", "", "4", "", s2);
-	printf("FT_PRINTF\n");
-	ft_printf(" %s %s %s %s %s\n", " - ", "", "4", "", s2);
-}
- */
