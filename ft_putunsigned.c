b665eb49e4cf6d902b333e8d09c325b4775518fd
manonlebihan <manonlebihan1999@gmail.com>
2022-08-19 14:53:14 +0200

    %u, OK

--- ft_putunsigned.c
+++ ft_putunsigned.c
@@ -6,7 +6,7 @@
 /*   By: mle-biha <mle-biha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 11:39:44 by mle-biha          #+#    #+#             */
-/*   Updated: 2022/08/18 21:03:58 by mle-biha         ###   ########.fr       */
+/*   Updated: 2022/08/19 14:52:26 by mle-biha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,15 @@
 
 int	ft_putunsigned(unsigned int n)
 {
+	int	i;
+
+	i = 0;
 	if (n < 10)
-		ft_putchar('0' + n);
+		i += ft_putchar('0' + n);
 	else
 	{
-		ft_putunsigned(n / 10);
-		ft_putunsigned(n % 10);
+		i += ft_putunsigned(n / 10);
+		i += ft_putunsigned(n % 10);
 	}
-	return (1);
+	return (i);
 }
